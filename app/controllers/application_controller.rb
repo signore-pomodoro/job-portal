5569d579e397d1566e2482e1f06aec35e02d46b1
signore-pomodoro <riudo@list.ru>
2022-02-11 12:37:42 +0300

    only signed_in employer can edit his job

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_employer_can_edit?
+
+  def current_employer_can_edit?(job)
+    employer_signed_in? && job.employer == current_employer
+  end
 end
